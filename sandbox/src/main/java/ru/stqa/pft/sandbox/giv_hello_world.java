695a922d55a45c73f5df0d23e8c989340d278881
Bodyarm <i.v.gen@yandex.ru>
2018-07-08 21:06:15 +0300

    Создан пакет для моей первой програмы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/giv_hello_world.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/giv_hello_world.java
@@ -1,6 +1,9 @@
-public class l1_m3_hello_world {
+package ru.stqa.pft.sandbox;
+
+public class giv_hello_world {
 	
 	public static void main(String[] args) {
+
 		System.out.println("Hello, world!");
 	}
 
