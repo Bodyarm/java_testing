32911b5846beb85a75640b9b5d8e77de34b0917b
Bodyarm <i.v.gen@yandex.ru>
2018-07-10 23:51:25 +0300

    Изменение форатирования

--- sandbox/src/main/java/ru/stqa/pft/sandbox/giv_hello_world.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/giv_hello_world.java
@@ -1,10 +1,9 @@
 package ru.stqa.pft.sandbox;
 
 public class giv_hello_world {
-	
-	public static void main(String[] args) {
 
-		System.out.println("Hello, world!");
-	}
+  public static void main(String[] args) {
+      System.out.println("Hello, world!");
+  }
 
 }
