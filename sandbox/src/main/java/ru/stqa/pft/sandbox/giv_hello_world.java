037d6949602b67aee57b312b473c498fc4245447
Bodyarm <i.v.gen@yandex.ru>
2018-07-11 21:43:53 +0300

    Добавлены функции

--- sandbox/src/main/java/ru/stqa/pft/sandbox/giv_hello_world.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/giv_hello_world.java
@@ -3,7 +3,33 @@ package ru.stqa.pft.sandbox;
 public class giv_hello_world {
 
   public static void main(String[] args) {
-      System.out.println("Hello, world!");
+    String text = "world";
+    System.out.println("Hello, "+ text + " & world!");
+    hello();
+    hello2("Karl");
+    double l = 8;
+    double s = l* l;
+    System.out.println("Площадь квадрата со стороной "+l+ " = "+ s);
+    double lll = 5;
+    System.out.println("Square of a square with len = " + lll + " equal " + area(lll));
+
+    double a = 4;
+    double b =9 ;
+    System.out.println("Пдощадь прямоугольника = " + area(a,b));
   }
 
+  public static void hello(){
+    System.out.println("Hello World!");
+  }
+
+  public static void hello2(String somebody){
+    System.out.println("Hello, " +somebody + "!" );
+  }
+
+  public static double area(double len){
+    return len*len;
+  }
+  public static double area(double a, double b){
+    return a*b;
+  }
 }
