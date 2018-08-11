2ff8030dc2c0944ef539141f0f30a1ea8351ae93
Bodyarm <i.v.gen@yandex.ru>
2018-08-12 00:59:39 +0300

    3.9 оптимизация переходов на страницы
    Изменения в NavigationHelper.java

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -7,6 +7,7 @@ public class ContactCreationTests extends TestBase{
 
   @Test
   public void testContactCreation() {
+    app.getNavigationHelper().gotoHomePage();
     app.getContactHelper().initContactCreation();
     app.getContactHelper().fillContactForm(new ContactData("Test", "Test Middlename", "Test LastName", "Test NickName", "Test Title", "Test Company", "Test address", "999", "921", "812", "499", "test@test.ru", "test@mail.ru", "test@yandex.ru", "Test address2","test1"),true);
     app.getContactHelper().submitContactCreation();
