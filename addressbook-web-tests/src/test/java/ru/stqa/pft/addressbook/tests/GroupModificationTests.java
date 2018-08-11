8ee01043b504689cb9a8178498c6a2d0c91c6b2b
Bodyarm <i.v.gen@yandex.ru>
2018-08-12 01:47:09 +0300

    3.10 add pre-check for GroupMod and GroupDel tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -9,7 +9,10 @@ public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification() {
     app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().selectGroup();;
+    if (! app.getGroupHelper().isThereAGroup()){
+     app.getGroupHelper().createGroup(new GroupData("test1",null,null));
+    }
+    app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("mod1","mod2","mod3"));
     app.getGroupHelper().submitGroupModification();
