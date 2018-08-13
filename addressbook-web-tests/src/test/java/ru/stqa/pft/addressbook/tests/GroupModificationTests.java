ff9f2e2eca05c12bce53ee65b44cdb20825a3c5d
Bodyarm <i.v.gen@yandex.ru>
2018-08-13 21:23:23 +0300

    4.7 add compaire collections for GroupModificationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -15,14 +16,22 @@ public class GroupModificationTests extends TestBase {
     if (! app.getGroupHelper().isThereAGroup()){
      app.getGroupHelper().createGroup(new GroupData("test1",null,null));
     }
+
     List<GroupData> before = app.getGroupHelper().getGroupList();
+
     app.getGroupHelper().selectGroup(before.size() -1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("mod1","mod2","mod3"));
+    GroupData mgroup = new GroupData(before.get(before.size()-1).getId(),"mod1","mod2","mod3");
+    app.getGroupHelper().fillGroupForm(mgroup);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
+
     List<GroupData> after = app.getGroupHelper().getGroupList();
+
     Assert.assertEquals(before.size(),after.size());
 
+    before.remove(before.size()-1);
+    before.add(mgroup);
+    Assert.assertEquals(new HashSet<>(before),new HashSet<>(after));
   }
 }
