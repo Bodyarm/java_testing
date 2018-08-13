b628e643f8846da8f95d5bcfe5bcf279793dabae
Bodyarm <i.v.gen@yandex.ru>
2018-08-13 21:37:12 +0300

    4.8 add compaire collection for GroupCreationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase{
@@ -12,9 +13,20 @@ public class GroupCreationTests extends TestBase{
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    GroupData cgroup = new GroupData("test1", null, null);
+    app.getGroupHelper().createGroup(cgroup);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(before.size(),after.size() - 1);
+
+
+
+    int max = 0;
+    for (GroupData g: after){
+      if (g.getId()>max) { max = g.getId();}
+    }
+    cgroup.setId(max);
+    before.add(cgroup);
+    Assert.assertEquals(new HashSet<>(before),new HashSet<>(after));
   }
 
 }
