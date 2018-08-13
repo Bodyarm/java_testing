44b0b9977f3d3343b572c1e8ec76ac37601702ce
Bodyarm <i.v.gen@yandex.ru>
2018-08-13 21:02:47 +0300

    4.6 add compaire group collection on GroupDeletionTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -14,12 +14,18 @@ public class GroupDeletionTests extends TestBase {
     if (! app.getGroupHelper().isThereAGroup()){
       app.getGroupHelper().createGroup(new GroupData("test1",null,null));
     }
+
     List<GroupData> before = app.getGroupHelper().getGroupList();
+
     app.getGroupHelper().selectGroup(before.size() -1);
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
+
     List<GroupData> after = app.getGroupHelper().getGroupList();
+
     Assert.assertEquals(before.size(),after.size() +1);
+    before.remove((before.size() -1));
+    Assert.assertEquals(before,after);
   }
 
 }
